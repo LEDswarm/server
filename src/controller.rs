224b47a63681dafd10337a7e9e860f35bcc89f91
Moritz Riede <moritz@mriede.com>
2023-01-22 06:34:44 +0100

    feat: start creating a data structure for our controllers

---
+++ src/controller.rs
@@ -0,0 +1,6 @@
+pub struct Controller {
+    /// The MAC address of the controller.
+    pub mac_address: String,
+    /// The network address of the controller.
+    pub network_address: String,
+}
